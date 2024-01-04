76e928431c18045aa8bb2bebd14fcf87f1b0a677
Lucas-Perdigao <lmperdigao@hotmail.com>
2024-01-04 20:35:27 -0300

    feat: adding UserSevice

---
+++ src/modules/user/services/user-service.ts
@@ -0,0 +1,29 @@
+import { IUserRepository } from "../repository/user-repository-interface";
+
+class UserService {
+    constructor(private userRepository: IUserRepository){}
+
+    async getAll(){
+
+    }
+
+    async getByEmail(){
+
+    }
+
+    async getById(){
+
+    }
+
+    async create(){
+
+    }
+
+    async update(){
+
+    }
+
+    async softDelete(){
+        
+    }
+}
